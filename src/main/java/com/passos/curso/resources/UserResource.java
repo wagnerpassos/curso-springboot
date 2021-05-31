6e5a491372cfaa590ac5a684a60aa138bda3e535
Wagner Passos <wagner_passos@hotmail.com>
2021-05-31 19:11:19 -0300

    Adicionada as camadas de service e Registro de componente

--- src/main/java/com/passos/curso/resources/UserResource.java
+++ src/main/java/com/passos/curso/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.passos.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.passos.curso.entities.User;
+import com.passos.curso.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User user = new User(1L, "wagner", "wagner@gmail.com", "13131313", "senha");
+	public ResponseEntity<List<User>> findAll(){
+		List<User> users = service.findAll();
+		
+		return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value ="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 }
